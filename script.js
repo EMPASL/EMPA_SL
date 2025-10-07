f16103fb85447666e62e2d161f843371e78fae52
EMPASL <alejandroas2006@gmail.com>
2025-10-07 10:40:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("form-contacto").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Gracias por contactarnos. Te responderemos pronto.");
+  this.reset();
+});
